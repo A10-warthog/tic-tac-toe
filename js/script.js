4fd718826a5b2ac32c618ba13399afd106acc8f5
A10-warthog <behemoth11111111@gmail.com>
2023-02-20 23:23:51 +0530

    Add methods to change, reset and view array

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-const Gameboard = (() => {
-  const gameboard = [];
-  const getBoard = () => gameboard();
-
-  return { getBoard };
+// GameBoard module
+const GameBoard = (() => {
+  const gameBoard = [];
+  gameBoard.length = 9;
+  gameBoard.fill(null);
+  const getBoard = () => [...gameBoard];
+  const printBoard = () => console.log(gameBoard);
+  const resetBoard = () => gameBoard.fill(null);
+  const updateBoard = (mark, index) => {
+    gameBoard[index] = mark;
+  };
+  return { getBoard, printBoard, resetBoard, updateBoard };
 })();
