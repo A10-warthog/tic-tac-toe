39d2086c6857240ca0898a1e58f4da281a0f96ec
A10-warthog <behemoth11111111@gmail.com>
2023-02-25 16:08:17 +0530

    Add event listener to restart and reset button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-useless-return */
 // GameBoard module
-
 const DOM = (() => {
   return {
-    dataName: document.querySelectorAll('[data-name]'),
-    gameBox: document.querySelector('.game__box'),
-    gamePlay: document.querySelector('.game__play'),
-    gameChoice: document.querySelector('.game__choice'),
-    gameEndBtn: document.querySelector('.game__end'),
-    gameResult: document.querySelector('.game__result'),
-    currentPlayer: document.querySelector('.game__current'),
-    gameBtn: this.gamePlay.querySelectorAll('button'),
+    dataName: document.querySelectorAll("[data-name]"),
+    gameBox: document.querySelector(".game__box"),
+    gamePlay: document.querySelector(".game__play"),
+    gameChoice: document.querySelector(".game__choice"),
+    gameEndBtn: document.querySelector(".game__end"),
+    gameResult: document.querySelector(".game__result"),
+    currentPlayer: document.querySelector(".game__current"),
+    gameBtn: this.gamePlay.querySelectorAll("button"),
     restartBtn: this.gameEndBtn.firstElementChild,
     eListen(elm, type, func) {
       elm.addEventListener(type, func);
@@ -29,13 +28,13 @@ const DOM = (() => {
     },
     render(board) {
       for (let i = 0; i < gameBtn.length; i++) {
-        if (board[i] === null) gameBtn[i].textContent = '';
+        if (board[i] === null) gameBtn[i].textContent = "";
         else gameBtn[i].textContent = board[i];
       }
     };
   };
 })();
-
+// GameBoard module
 const GameBoard = (() => {
   const gameBoard = Array(9).fill(null);
   const getBoard = () => [...gameBoard];
@@ -64,8 +63,8 @@ const Player = (name, type, mark) => {
 
 // Controller module
 const Controller = (() => {
-  const player1 = Player('Player 1', '', 'X');
-  const player2 = Player('Player 2', '', 'O');
+  const player1 = Player("Player 1", "", "X");
+  const player2 = Player("Player 2", "", "O");
   const player = {};
   const active = [player1];
   const updateBoard = (player, index) => {
@@ -88,11 +87,13 @@ const Controller = (() => {
     ];
     for (let i = 0; i < cases.length; i++) {
       const [a, b, c] = cases[i];
-      if (board[a] === board[b] && board[a] === board[c]) return player[board[a]];
+      if (board[a] === board[b] && board[a] === board[c])
+        return player[board[a]];
     }
   };
   // change player's turn
-  const playerTurn = (player) => (player.getName() === player1.getName() ? player2 : player1);
+  const playerTurn = (player) =>
+    player.getName() === player1.getName() ? player2 : player1;
   // only change active player on condition true
   const takeTurn = (player, val) => {
     const returnPlayer = updateBoard(player, val);
@@ -107,31 +108,56 @@ const Controller = (() => {
   // helper function to remove event
   const onBtnClick = (event) => {
     const btn = event.target;
-    const id = DOM.elmAttr(btn, 'data-id');
+    const id = DOM.elmAttr(btn, "data-id");
     takeTurn(active[0], +id);
     playRound();
   };
   // gets id from target btn
-  const humanTurn = () => DOM.eListen(DOM.gamePlay, 'click', onBtnClick);
+  const humanTurn = () => DOM.eListen(DOM.gamePlay, "click", onBtnClick);
   const aiTurn = () => takeTurn(active[0], lowAiMove());
   //start of game
   const playRound = () => {
     const roundResult = isWinner(GameBoard.getBoard());
     if (roundResult !== undefined) {
       DOM.gameResult.textContent = `Winner is ${roundResult}`;
-      DOM.classToggle(DOM.restartBtn, 'game--hidden');
-      DOM.eRemove(DOM.gamePlay, 'click', onBtnClick);
+      DOM.classToggle(DOM.restartBtn, "game--hidden");
+      DOM.eRemove(DOM.gamePlay, "click", onBtnClick);
       playAgain();
       return;
     } else if (GameBoard.getBoard().every((elm) => elm !== null) === true) {
-      DOM.classToggle(DOM.restartBtn, 'game--hidden');
+      DOM.classToggle(DOM.restartBtn, "game--hidden");
       DOM.gameResult.textContent = "It's a draw";
       playAgain();
       return;
     } else {
-      DOM.currentPlayer.textContent = 'Turn :' + active[0].getName();
-      if (active[0].getType() === 'human') humanTurn();
-      if (active[0].getType() === 'ai') aiTurn();
+      DOM.currentPlayer.textContent = "Turn :" + active[0].getName();
+      if (active[0].getType() === "human") humanTurn();
+      if (active[0].getType() === "ai") aiTurn();
     }
   };
+  // option for play mode
+  DOM.eListen(DOM.gameChoice, "click", (event) => {
+    let type = DOM.elmAttr(event.target, "data-type");
+    if (type === null) return;
+    type = type.split(" ");
+    player1.setType(type[0]);
+    player2.setType(type[1]);
+    DOM.classToggle(DOM.gameBox, "game--none");
+    DOM.classToggle(DOM.restartBtn, "game--hidden");
+    // starts the round
+    playRound();
+  });
+  // button to reset and restart game
+  DOM.eListen(DOM.gameEndBtn, "click", (event) => {
+    const btn = event.target;
+    if (DOM.elmAttr(btn, "data-name") === "reset") {
+      DOM.classToggle(DOM.gameChoice, "game--none");
+      DOM.classToggle(DOM.gameBox, "game--none");
+    }
+    playAgain();
+    DOM.render(GameBoard.getBoard());
+    DOM.gameResult.textContent = "\xA0";
+    DOM.classAdd(DOM.restartBtn, "game--hidden");
+    if (DOM.elmAttr(btn, "data-name") === "restart") playRound();
+  });
 })();
